03aaec24ef3805ce0cc64048203a70161a98c8e0
Sumit Mittal <sumitmittal.adonis@gmail.com>
2026-05-13 06:47:34 -0400

    Default Prompts (System, User, Assitant, Tool) in Spring AI

---
+++ 5-SpringAI-Prompts/src/main/java/com/sumit/ai/controller/ChatController_defaultSystemPrompt.java
@@ -0,0 +1,34 @@
+package com.sumit.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController_defaultSystemPrompt {
+
+    private final ChatClient chatClient;
+
+    public ChatController_defaultSystemPrompt(ChatClient.Builder ccBuilder ){
+        this.chatClient = ccBuilder
+                .defaultSystem("""
+                      You are an internal HR assistant. Your role is to help employees with questions related \s
+                      to HR policies, such as leave policies, working hours, benefits, and code of conduct. \s
+                      If a user asks for help with anything outside of these topics, kindly inform them that \s
+                      you can only assist with queries related to HR policies.
+                      """)
+                .build();
+    }
+
+    @GetMapping("/chat_defaultSystemPrompt")
+    public String chat(@RequestParam("message") String message){
+        return chatClient
+                .prompt()
+                .user(message)
+                .call().content();
+    }
+
+}
