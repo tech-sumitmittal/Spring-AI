5611ef988a32d20cd630b80c8887b1f25e604a66
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-05-16 19:37:47 -0400

    Spring AI : Added RAG basic setup in Spring-AI project

---
+++ 7-SpringAI-RAG/src/main/java/com/sumit/ai/config/ChatClient_with_in_memory_chat_memory.java
@@ -0,0 +1,44 @@
+package com.sumit.ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatClient_with_in_memory_chat_memory {
+
+    @Bean("ChatClient_with_in_memory_chat_memory")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder,
+                                 @Qualifier("inMemoryChatMemory") ChatMemory chatMemory) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
+                .build();
+    }
+
+    // to override default behavior of MessageWindowCM
+    @Bean("inMemoryChatMemory")
+    ChatMemory chatMemory(InMemoryChatMemoryRepository inMemoryCMR) {
+        return MessageWindowChatMemory.builder()
+                .maxMessages(3)                     // will remember last 3 messages
+                .chatMemoryRepository(inMemoryCMR)
+                .build();
+    }
+
+    @Bean
+    public InMemoryChatMemoryRepository inMemoryChatMemoryRepository() {
+        return new InMemoryChatMemoryRepository();
+    }
+
+}
